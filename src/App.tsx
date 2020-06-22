253f4a9e989f2ac54e7fca581800c5305118460d
devnikop <devnikop+github@gmail.com>
2020-06-22 10:32:08 +0300

    add typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const App = () => {
+  return (
+    <StyledApp>
+      <article></article>
+    </StyledApp>
+  );
+};
+
+const StyledApp = styled.header``;
+
+export default App;
